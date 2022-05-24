9404b9de8afcc2ef12d45a0135015d8a3459accb
AleIHC <ale.shc218@gmail.com>
2022-05-24 12:04:14 -0400

    validacion notas

--- src/com/generation/f20220524/CalculoPromedio.java
+++ src/com/generation/f20220524/CalculoPromedio.java
@@ -9,7 +9,7 @@ public class CalculoPromedio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Integer[] notas = {98, 92, 995, 99, 99, 99, 63, 95, 34, 78, 87, 97};
+		Integer[] notas = {98, 92, 99, 99, 99, 99, 63, 95, 34, 78, 87, 97};
 		
 		
 		//variable acumuladora
@@ -20,27 +20,37 @@ public class CalculoPromedio {
 			//validar la nota
 			if(notas[i] < 0 || notas[i] > 100) {
 				System.out.println("La nota: " + notas[i] + " en la posición " + (i +1)+ " está fuera de rango");
+				notasFueraDeRango++;
 			}
 			
 			sumaNotas = sumaNotas + notas[i];
 		}
 		
+		if(notasFueraDeRango == 0) {//
+			//promedio -> suma todas las notas / cantidad de notas
+			float promedio =sumaNotas / notas.length;
+				
+			//Suma de notas
+			System.out.println("suma de notas: " + sumaNotas);
+			
+			//Promedio 
+			System.out.println("El promedio es: " + promedio);
 		
-		System.out.println("suma de notas: " + sumaNotas);
-		
-		//promedio -> suma todas las notas / cantidad de notas
-		float promedio =sumaNotas / notas.length;
-		
-		
-		//Punto critico
-		if(promedio >= 50 && promedio <=100) {
-			System.out.println("Apruebo");
-		
-		} else if(promedio <= 0 && promedio <= 49) {
-			System.out.println("Repruebo");
+			
+			//Punto critico
+			if(promedio >= 50 && promedio <=100) {
+				System.out.println("Apruebo");
+			
+			} else if(promedio <= 0 && promedio <= 49) {
+				System.out.println("Repruebo");
+			} else {
+				System.out.println("Error");
+			}
 		} else {
-			System.out.println("Revisar notas");
+			System.out.println("Favor revisar notas");
 		}
+		
+		
 		
 	
 	}
